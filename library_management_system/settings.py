8798ca29c91dc30cd2fd16c97a0480ff605ece14
mowuj <mowuj92@gmail.com>
2024-01-11 21:03:25 +0600

    solve static problem

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 import dj_database_url
 import environ
 from pathlib import Path
@@ -132,14 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    BASE_DIR /'static',
-]
 MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
-
-# Path where media is stored'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
